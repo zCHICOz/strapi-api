a52aa15a167e795df5b85a1ca5dda1f89862f7b3
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-02-16 16:05:55 -0300

    v1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3d3dcf12ef04717bfd1b172055f43bfe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10d88b08-778f-4a5f-9000-c53d31b7da86'
+};
